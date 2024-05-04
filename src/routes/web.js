be5d7893a69b9e0579ba8f4d43fd6fe4879175d2
NguyenTienPhong <321phong@gmail.com>
2024-05-04 13:21:22 +0700

    4/5/2024

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {getHomepage} = require('../controllers/homeController.js')
 
 // route init
 router.get('/', (req, res) => {
     // res.send('Hello World!')
     res.render('sample.ejs')
    })
+
+router.get('/a',getHomepage)   
 
 module.exports = router;
