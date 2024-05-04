d5d45c6348bed28abe12dc90df64ace4643125a6
NguyenTienPhong <321phong@gmail.com>
2024-05-04 16:24:32 +0700

    vai lon luon

--- src/sever.js
+++ src/sever.js
@@ -15,15 +15,8 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web.js');
 app.use('/', webRoutes);
 
-//mysql
-const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database:process.env.DB_NAME
-})
+//database
+const connection=require('./config/database.js')
 connection.query(
   'select * from Users u',
   function(err,results,fields){
